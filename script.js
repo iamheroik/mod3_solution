1f13f9e029e5a43d92a76f021fa77cdaf0a47419
iamheroik <nicholas@heroikmedia.com>
2025-04-15 11:13:38 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  // Same-page navigation when clicking a menu item
-  $("#collapsable-nav a").click(function(event) {
-    // Collapse navbar when a link is clicked
-    $("#collapsable-nav").collapse('hide');
-  });
-});
